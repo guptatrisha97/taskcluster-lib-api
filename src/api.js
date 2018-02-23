b7bcc72eca789395b1f7869b416e50748985998f
guptatrisha97 <35959848+guptatrisha97@users.noreply.github.com>
2018-02-23 23:20:39 +0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -782,6 +782,11 @@ API.prototype.declare = function(options, handler) {
     options.output = this._options.schemaPrefix + options.output;
   }
   this._entries.push(options);
+  var valueArr = values.map(function(item){ return item.name });
+  var isDuplicate = valueArr.some(function(item, idx){ 
+   return valueArr.indexOf(item) != idx 
+});
+throw “This function has already been declared”;
 };
 
 /**
